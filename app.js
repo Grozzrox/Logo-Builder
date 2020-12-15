5879a8949a87a63f4f8b4763ce2a9766d864cdc6
Andrew <grozzy09@gmail.com>
2020-12-15 00:02:01 -0500

    15Dec2020_1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ form.addEventListener("submit", function(e) {
 
     const newLogo = createLogo(name.value, color.value, size.value);
     text.appendChild(newLogo);
-})
+});
 
 function createLogo(name, color, size) {
     const logo = document.createElement("h2");
@@ -18,4 +18,35 @@ function createLogo(name, color, size) {
     logo.style.color = color;
     logo.style.fontSize = `${size}px`;
     return logo;
+};
+
+document.addEventListener("keypress", function(e) {
+    console.log(e.key);
+});
+
+const friendsList = document.querySelectorAll("li button");
+const newFriend = document.querySelector("#friend");
+const friendForm = document.querySelector("#newfriend");
+const list = document.querySelector("#friendslist");
+
+
+friendForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const newLi = document.createElement("li");
+    const newBtn = document.createElement("button");
+    newLi.innerText = `${newFriend.value} `;
+    newBtn.innerText = "UnFriend"
+    newBtn.addEventListener("click", function(e) {
+        e.target.parentElement.remove();
+    })
+    newFriend.value = "";
+    newLi.appendChild(newBtn);
+    list.appendChild(newLi);
+
+})
+
+for (let friend of friendsList) {
+    friend.addEventListener("click", function(e) {
+        e.target.parentElement.remove();
+    })
 }
